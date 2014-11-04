2ffaeebc55ef2b4caf3a9da14b5123bb2341da05
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-03 20:12:17 -0500

    Updated script.js
    Continued taking notes as js comments and testing adding variables.

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,5 +1,14 @@
 // This is the js file for the Expressions Screencast
 
 
-var a = 2; // defining and declaring a as 2
+var a = 2; // declaring a as variable and defining as 2
+var b;
+b = a + 3; // adds 3 to a
+// Commenting this out a + 3;
+// Originally, the line above added 3 to the "a" variable but does not store it
+
+//if we want to add to a and change it, we need to use an assignment operator
+
+// Commenting out a = a + 3; We need to add an assignment
 console.log(a); // printing a in the console
+console.log(b); // printing b in the console
