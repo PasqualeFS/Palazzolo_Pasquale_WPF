51d92cab99993fa4c08c3a5766f6309deb80e9b6
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 13:10:32 -0500

    Used decrementing
    added - - operator (without spaces in my code but GUI converts two
    minus signs together to an emdash).

--- Expressions/Assignment_Operators/js/script.js
+++ Expressions/Assignment_Operators/js/script.js
@@ -1,7 +1,7 @@
 // This is the JS file to go along with the Assignment Operators Screencast
 
 var a = 3;
-a ++ ; // this is the same as if we had written a = a + 1 or a += 1
+a -- ; // this is the same as if we had written a = a - 1 or a -= 1
 console.log(a);
 
 /*
