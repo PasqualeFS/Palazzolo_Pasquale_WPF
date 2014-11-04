7eb67b6ab989167cabdb54f00b9aba619fa984be
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 08:35:37 -0500

    Test 2
    Divided sum of quiz numbers by 4. Tested in browser.  Wrong answer.

--- Expressions/Order_Of_Operations/js/script.js
+++ Expressions/Order_Of_Operations/js/script.js
@@ -8,5 +8,5 @@ var quiz4 = 80;
 
 // the average is the sum of the items divided by the number of items
 
-var average = quiz1 + quiz2 + quiz3 + quiz4;
-console.log(average);
+var average = quiz1 + quiz2 + quiz3 + quiz4/4;
+console.log(average); 
