3511ff77561913071214c52535592574aabfbb89
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 12:44:58 -0500

    Added vars and concatenated
    Created three new variables. Since these are numbers, they added
    together in the browser test.  Concatenation was not achieved.

--- Expressions/Casting/js/script.js
+++ Expressions/Casting/js/script.js
@@ -1,5 +1,14 @@
 // This is the JS file to go along with the Casting Screencast
 
+/*
 var stringVar = "6"; // Declaring the character 6 (not the number) as a variable.
 var result = 7 + Number(stringVar); // This is attempting to add a number to a string.
 console.log(result); // Printing the result to the console to verify.
+*/
+
+var areaCode = 407;
+var firstPart = 555;
+var lastPart = 1234;
+
+var phoneNo = areaCode + firstPart + lastPart; // this will add three variables together instead of concatenating
+console.log(phoneNo); // prints to the console
