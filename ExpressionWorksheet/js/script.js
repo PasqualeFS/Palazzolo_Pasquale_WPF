af67bf74ebcf8af685c07e72efbc2f3ab5707e3b
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 17:20:14 -0500

    Added Problem Two
    Sorted out the issues with dividing up the pizza.  Used console.log to
    show in browser.  Cross-referenced the console output with a calculator
    and it is working correctly.

--- ExpressionWorksheet/js/script.js
+++ ExpressionWorksheet/js/script.js
@@ -14,9 +14,9 @@ Calculate how old Sparky the pit bull is in dog years based on his actual age.
 var humanYears = 3; // This is Sparkys' age in human years
 var dogYearsMultiplier = 7; // Dogs age 7 years for every human year.  This will provide a x7 multiplier in the final result
 
-var result = "Sparky is " + humanYears + " human years old which is " + humanYears * dogYearsMultiplier + " in dog years.";
+var dogResult = "Sparky is " + humanYears + " human years old which is " + humanYears * dogYearsMultiplier + " in dog years.";
 // The line above includes strings, proper spacing, integration of the variables and multiplication of variables where needed.
-console.log(result); // This should print the result of 21 to the console when browser testing.
+console.log(dogResult); // This should print the result of 21 to the console when browser testing.
 
 /* PROBLEM 2: Slice of Pie Part I
  A bunch of students are having a party and somebody ordered pizza.
@@ -25,6 +25,8 @@ console.log(result); // This should print the result of 21 to the console when b
  really precise, so this can be a decimal, like 3.52 slices, etc.)
  */
 
+var pizzasOrdered = 10; // This states that 10 pizzas were ordered by somebody
 var slicesPerPie = 12; // This states that each pizza pie has 12 slices
-var partyRSVPS = 18 // This states that 18 people are attending this party
-
+var partyRSVPS = 18; // This states that 18 people are attending this party
+var pizzaResult = "Each person ate " + (pizzasOrdered * slicesPerPie) / partyRSVPS + " slices of pizza at the party.";
+console.log(pizzaResult);
