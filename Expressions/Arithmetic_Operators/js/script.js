a33f13c8a0f3be85ef666f7715adf93b1c06473a
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-03 20:48:03 -0500

    Completed Arithmetic Operators
    Added proper symbols to divide by half the width times the height.
    Tested result in console.log and achieved correct area of a triangle.

--- Expressions/Arithmetic_Operators/js/script.js
+++ Expressions/Arithmetic_Operators/js/script.js
@@ -4,6 +4,5 @@
 
 var width = 4; // This would actually be the full width of a rectangle
 var height = 5; // This is the full height of a rectangle
-var area = width * height;
-
-console.log(area);
+var area = width/2 * height; //This carries out the correct division and multiplication to acquire the triangle dimensions
+console.log(area); // This prints the result in the console
