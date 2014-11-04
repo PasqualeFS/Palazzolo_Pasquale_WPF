8f6a17709160e1b125869d9356ebdc3d0d6814a9
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 12:26:56 -0500

    Added new variables
    Added two variables with numbers as strings. Third variable result will
    add the first two variables.  Tested in browser.  Result read the
    numbers as text since they’re strings and concatenated them together
    instead of adding.

--- Expressions/Concatenating_Strings/js/script.js
+++ Expressions/Concatenating_Strings/js/script.js
@@ -1,6 +1,13 @@
 // This is the JS file to go along with the Concatenating Strings Screencast
 
+/*
 var firstName = "Pasquale";
 var lastName = "Palazzolo";
 var fullName = firstName + " " + lastName;
 console.log(fullName);
+*/
+
+var a = "6";
+var b = "7";
+var result = a + b;
+console.log(result);
