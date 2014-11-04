57936385499c6b03980d83a306f2b24675514862
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-03 19:42:48 -0500

    Added names and printed
    Listed Muppet names and tested with console.log

--- Arrays/js/script.js
+++ Arrays/js/script.js
@@ -1,1 +1,6 @@
 // This is the js file for the Arrays Screencast
+
+var muppetNames = [ "Beaker", "Sweetums", "Animal"]; // Lists are enclosed in square brackets
+// The index for this array begins with 0. Example: Beaker = 0, Sweetums = 1, Animal = 2
+
+console.log(muppetNames);
