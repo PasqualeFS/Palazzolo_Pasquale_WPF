125f6341d833c8d1d22f4c19ba39434c9b0ce596
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-03 19:22:47 -0500

    Updated comments
    Added additional comments about variables and spacing

--- Defining Variables/js/script.js
+++ Defining Variables/js/script.js
@@ -5,3 +5,7 @@ age = 39; //definition
 //storage container = value
 
 var yearOfBirth = 1970; //declaring and defining on the same line
+
+// if declaring separately from defining, make sure the declaration comes first in your code
+// spacing is a formality.  Verticle or horizontal space doesn't really matter.  Keep it neat so it all makes sense to
+// you and to others who may read your code in the future.
