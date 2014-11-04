1fe5cc3fcb29afb63e4b54d521c6a4406002e0fb
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-03 19:10:49 -0500

    Added comments and variables
    Took notes on this screencast and placed them in the js file as
    comments. Created live example variables to go along with the notes.

--- Declaring Variables/js/script.js
+++ Declaring Variables/js/script.js
@@ -1,1 +1,23 @@
 // This is the js file for the Declaring Variables Screencast
+
+var age;
+
+// Never start a variable with a number.  You can start it with...
+
+var age2;
+
+//...but never start it with...
+
+var 2age;
+
+
+// Fine class examples follow
+var num;
+var foo;
+var bar;
+
+// Use meaningful variable names going forward
+// Use camelCase where the second word is capitalized
+
+var pasqualePalazzolo;
+var declaringVariables;
