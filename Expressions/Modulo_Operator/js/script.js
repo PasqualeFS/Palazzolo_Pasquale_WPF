b1fa0c005bb40d8b9735095d278636b9e2592f52
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-03 20:56:13 -0500

    Added variable, division and modulo
    Created remainder variable and tried to division.  Changed division to
    modulo and tested in browser.

--- Expressions/Modulo_Operator/js/script.js
+++ Expressions/Modulo_Operator/js/script.js
@@ -1,1 +1,9 @@
 // Expressions - Modulo Operator
+
+/* 
+var remainder = 32 / 10; // declared variable as 32 divided by 10
+console.log(remainder); // printed in console. This did not show us the remainder
+*/
+
+var remainder = 32 % 10; // declared variable again using the modulo operator
+console.log(remainder); // printed in console and was shown a remainder of 2
