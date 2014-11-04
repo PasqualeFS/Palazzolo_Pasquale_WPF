f7d4ba7e16e9d4c9acb849a20624d5db35d7a288
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-03 20:22:56 -0500

    Added comments
    Took more notes to go along with the screencast and added them as
    comments.

--- Expressions - Age Example/js/script.js
+++ Expressions - Age Example/js/script.js
@@ -1,5 +1,5 @@
 // This is the js file for the Expressions - Age Example Screencast
 
-var yearBorn = 1975; //The year I was born
-var age = 2014 - yearBorn;  //This subtracts my birth year from the current year and stores it as a variable
-console.log(age);
+var yearBorn = 1975; // The year I was born
+var age = 2014 - yearBorn;  // This subtracts my birth year from the current year and stores it as a variable
+console.log(age); // This prints age in the console
