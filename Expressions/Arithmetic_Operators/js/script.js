2aad38755773304fba5862d1be48b6683614a557
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-03 20:42:10 -0500

    Added multiplication test
    Added width and height variables. Added third variable to multiply
    these first two variables together. Tested in browser by printing
    result in the console.

--- Expressions/Arithmetic_Operators/js/script.js
+++ Expressions/Arithmetic_Operators/js/script.js
@@ -1,2 +1,9 @@
 // Javascript for Arithmetic Operators Screencast
 
+// area of a triangle is half the width times the height
+
+var width = 4; // This would actually be the full width of a rectangle
+var height = 5; // This is the full height of a rectangle
+var area = width * height;
+
+console.log(area);
