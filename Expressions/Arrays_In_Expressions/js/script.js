b0b171825f0eed922e051f13b2c4deec3c0d2767
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 08:53:15 -0500

    Added array and tested
    Tested the orangeBins array by adding and printing to the console but
    resulted in NaN

--- Expressions/Arrays_In_Expressions/js/script.js
+++ Expressions/Arrays_In_Expressions/js/script.js
@@ -1,1 +1,6 @@
 /* This JS file goes along with the Arrays in Expressions Screencast */
+
+var orangeBins = [234, 567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[3];
+console.log(total);
