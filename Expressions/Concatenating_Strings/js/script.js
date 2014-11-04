d575d6ac524b609ce898e9e303c43c7ed03ed093
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 12:21:00 -0500

    Added variables and tested
    added firstName, lastName and fullName. Tested in browser. Full name is
    concatenated but without spacing between first and last name.

--- Expressions/Concatenating_Strings/js/script.js
+++ Expressions/Concatenating_Strings/js/script.js
@@ -1,1 +1,6 @@
 // This is the JS file to go along with the Concatenating Strings Screencast
+
+var firstName = "Pasquale";
+var lastName = "Palazzolo";
+var fullName = firstName + lastName;
+console.log(fullName);
