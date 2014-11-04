1ac543b40cc7cc09bd3ce1a4faf6fbde669812ea
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-03 19:43:36 -0500

    Updated console.log
    Changed console.log to only print the first Muppet name in the array.

--- Arrays/js/script.js
+++ Arrays/js/script.js
@@ -3,4 +3,4 @@
 var muppetNames = [ "Beaker", "Sweetums", "Animal"]; // Lists are enclosed in square brackets
 // The index for this array begins with 0. Example: Beaker = 0, Sweetums = 1, Animal = 2
 
-console.log(muppetNames);
+console.log(muppetNames[0]);
