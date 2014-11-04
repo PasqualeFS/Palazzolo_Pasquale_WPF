c5fb8a0fa5dd7d233379c473ee9c2baea9b55e08
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 08:31:04 -0500

    Added variables
    Added four quiz variables and comments to explain how to calculate the
    average.

--- Expressions/Order_Of_Operations/js/script.js
+++ Expressions/Order_Of_Operations/js/script.js
@@ -1,1 +1,9 @@
 // This is the JS file to go along with the Order of Operations Screencast
+// Don't forget PEMDAS
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+// the average is the sum of the items divided by the number of items
