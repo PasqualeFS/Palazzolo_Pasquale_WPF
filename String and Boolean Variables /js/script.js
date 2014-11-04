96648befe736a6758ba90b2c156d301befc4fa2e
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-03 19:33:11 -0500

    Added variables and comments
    Created variables and comments with notes to support the teachings of
    this screencast

--- String and Boolean Variables /js/script.js
+++ String and Boolean Variables /js/script.js
@@ -1,3 +1,10 @@
 // This is the js file for the String and Boolean Variables Screencast
 
 var age = 39; //Number variable
+
+var isStudent = false; // Boolean Variable
+
+var firstName = "Pasquale"; // This is my name
+
+var phrase = 'You can\'t do that. \n Try using an escape character instead'; // By adding the backslash, it tells the machine to ignore the single quote that follows.
+// This ensures that the single quote of punctuation won't accidentally end the string.
