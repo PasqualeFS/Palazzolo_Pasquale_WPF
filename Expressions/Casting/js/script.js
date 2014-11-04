42bd81f8fc36b0e694aa41414ffebb2f64cec973
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 12:36:23 -0500

    Added variables
    These variables try to add a number to a string. Tested in browser.
    Result just put the two side by side but did not output the sum.

--- Expressions/Casting/js/script.js
+++ Expressions/Casting/js/script.js
@@ -1,1 +1,5 @@
 // This is the JS file to go along with the Casting Screencast
+
+var stringVar = "6"; // Declaring the character 6 (not the number) as a variable.
+var result = 7 + stringVar; // This is attempting to add a number to a string.
+console.log(result); // Printing the result to the console to verify.
