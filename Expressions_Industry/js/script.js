44ae5c524c27b283565995ba31f5a7ad0812e553
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 21:54:45 -0500

    Completed design and testing
    Finished working on HTML, CSS and JS files. JS has been tested in the
    browser and functions properly without the more detailed use of
    If/Then/Else statements. Too soon?

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,1 +1,12 @@
 // This is for my Industry Expressions assignment
+
+alert("You have 600px to work with. \n\nLet's calculate the remaining width of our email design space.\n\nYou have to use four TD elements across the width of the design.");
+
+var firstTD = prompt("How much is the width of your first TD element?");
+var totalSpace = 600;
+var firstRemainder = totalSpace - firstTD;
+var secondTD = prompt("You have " + firstRemainder + "px left to work with.\n\nHow much is the width of your second TD element?\nPlease make sure your entry is " + firstRemainder + " or less.");
+var secondRemainder = firstRemainder - secondTD;
+var thirdTD = prompt("You have " + secondRemainder + "px left to work with.\n\nHow much is the width of your third TD element?\nPlease make sure your entry is " + secondRemainder + " or less.");
+var thirdRemainder = secondRemainder - thirdTD;
+var fourthTD = prompt("BE CAREFUL!!! \n\nYou only have " + thirdRemainder + " px left to work with.\n\nPlease make sure your entry is exactly " + thirdRemainder + "px or your email design\nwill break!\n\nHow much is the width of your last TD element?");
