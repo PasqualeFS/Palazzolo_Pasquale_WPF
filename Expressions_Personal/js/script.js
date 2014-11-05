ebaa81cdeadd885a5cd7fb9097ddc1e2e350a312
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 20:32:58 -0500

    String to variable and final test
    Added a variable that holds the string of text combined with relevant
    variables.  The final alert displays the information back to the user.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,7 +1,8 @@
 // This is for my Personal Expressions assignment
 
 alert("Trans-Siberian Orchestra is playing at Amway Arena on 12/14/2014!"); // This line starts an alert to click through.
-var perPerson = 55.00; // This line stores the price per person for a single ticket to TSO
+var perPerson = 55.95; // This line stores the price per person for a single ticket to TSO
 var headCount = prompt("Tickets cost $" + perPerson + " per person. \nHow many people are in your party?"); //This line asks the user how many people are attending the TSO concert and stores the user data in a variable;
 var totalCost = headCount * perPerson; // This line multiplies the cost per person times the amount of people going.
-console.log(totalCost); 
+var displayTotal = "The total cost for " + headCount + " tickets is $" + totalCost + "."; // This creates a string concatenated with the relevant variables
+alert(displayTotal);
