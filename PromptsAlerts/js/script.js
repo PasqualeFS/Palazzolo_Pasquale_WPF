d623d08f44c68d2e9323c4b452400a1c5a0ae8a8
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 19:43:54 -0500

    Switched to alert
    Switched console.log to alert so the user can see the result.

--- PromptsAlerts/js/script.js
+++ PromptsAlerts/js/script.js
@@ -1,8 +1,15 @@
 // JS file to go along with Prompts & Alerts Screencast
 
+/*
 var userInput = prompt("Enter your year of birth:");
 var currentYear = 2014;
 var currentAge = currentYear - userInput;
 var ageDisplay = "You turn " + currentAge + " years old this year!";
 console.log(ageDisplay);
+*/
 
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height");
+var area = width * height;
+// console.log(area);
+alert(area);
