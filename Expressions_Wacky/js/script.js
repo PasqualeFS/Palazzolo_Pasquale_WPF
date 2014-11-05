1d119b214c69c43387399584b7251b7e9868345a
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 23:37:06 -0500

    Age calculation, ifs and comments
    updated age calculation, revised if/else statements and added comments
    throughout the document that show what everything does.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,33 +1,52 @@
 // This is the Javascript for my Wacky Expressions assignment
 
-var userName = prompt("What is your name?");
-var whoLike = prompt ("Who do you like?");
-var userNumber = prompt("Welcome to Ye Olde 1980's Fortune Teller! Please pick a number from 1 to 6.");
-// The prompt above will take a user number from 1 to 6 and store it in the variable "userNumber"
+var currentYear = 2014; // This declares the current year and stores it in a variable
+
+var userBirth = prompt("What year were you born?"); // This prompts the user for their year of birth and stores it in a variable
+
+var userAge = currentYear - userBirth; // This subtracts the user birth year from the current year and stores the result in a variable.
+
+/* The three variables above satisfy the calculation portion of the assignment.  The wacky part begins below and is just for fun as a throwback to one of my 1980s memories. */
+
+
+
+// The if and else statements that follow below are used to return various prompts to the user depending on their age.
+if (userBirth == 1999) {
+    var userNumber = prompt("Seriously? 1999? Did you party like it was? Anyway...\n\n" + userAge + " is a little young to remember the 1980s\n\nWe used to fool around with these paper fortune tellers as a joke! Please pick a number from 1 to 6.")
+}
+else if (userBirth <= 1989) {
+    var userNumber = prompt("The ripe old age of " + userAge + ". You should remember the 1980s well.\n\nWelcome to Ye Olde 1980's Fortune Teller! Please pick a number from 1 to 6.")
+}
+else {
+   var userNumber = prompt("You're a little young to remember the 1980s\n\nWe used to fool around with these paper fortune tellers as a joke! Please pick a number from 1 to 6.");
+}
+
+
+
+// The if and else statements below return a different prompt depending on the number the user chooses
+if (userNumber == 6) {
+    prompt("1, 2, 3, 4, 5, 6...Pick a color (red, blue, green, yellow) please:");
+} else if (userNumber == 5) {
+    prompt("1, 2, 3, 4, 5...Pick a color (red, blue, green, yellow) please:");
+} else if (userNumber == 4) {
+    prompt("1, 2, 3, 4...Pick a color (red, blue, green, yellow) please:");
+}  else if (userNumber == 3) {
+    prompt("1, 2, 3...Pick a color (red, blue, green, yellow) please:");
+} else if (userNumber == 2) {
+    prompt("1, 2...Pick a color (red, blue, green, yellow) please:");
+} else if (userNumber ==1) {
+    prompt("1...Pick a color (red, blue, green, yellow) please:");
+} else {
+    prompt("Oh, a smarty pants.  Clearly you can't count to six!\nLet's see if you can pick a color (red, blue, green, yellow)");
+}
+
+// The function below will redirect the page to the URL that's declared if the function is called in a line of javascript.
 
 function move() {
     window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 }
 
-if (userNumber == 6) {
-    var userColor = prompt("1, 2, 3, 4, 5, 6...Pick a color (red, blue, green, yellow) please:");
-} else if (userNumber == 5) {
-    var userColor = prompt("1, 2, 3, 4, 5...Pick a color (red, blue, green, yellow) please:");
-} else if (userNumber == 4) {
-    var userColor = prompt("1, 2, 3, 4...Pick a color (red, blue, green, yellow) please:");
-}  else if (userNumber == 3) {
-    var userColor = prompt("1, 2, 3...Pick a color (red, blue, green, yellow) please:");
-} else if (userNumber == 2) {
-    var userColor = prompt("1, 2...Pick a color (red, blue, green, yellow) please:");
-} else if (userNumber ==1) {
-    var userColor = prompt("1...Pick a color (red, blue, green, yellow) please:");
-} else {
-    var userColor = prompt("Oh, a smarty pants.  Clearly you can't count to six!\nLet's see if you can pick a color (red, blue, green, yellow)");
-}
 
-var openFlap1 = alert("Your fortune is " + userName + " and " + whoLike + " sittin\' in a tree. K-I-S-S-I-N-G!");
-var openFlap2 = alert(whoLike + " doesn't like you back. There are plenty of other fish in the sea.");
-var openFlap3 = alert("Your fortune is " + userName + " and " + whoLike + " sittin\' in a tree. K-I-S-S-I-N-G!");
-var openFlap4 = move();
+alert("Uh Oh! You've been Rick Rolled!"); // This warns the user that something ridiculous is about to happen.
 
-console.log(openFlap2)
+move(); // This simply calls the move function and redirects the user to Rick Astley doing his cool dance.
