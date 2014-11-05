80149a926b7dbb17080e6034657be9ece46fdd98
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 20:40:27 -0500

    Added console.log
    Assignment requires display of information in the console.  Added
    console.log for this reason.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,4 +5,5 @@ var perPerson = 55.95; // This line stores the price per person for a single tic
 var headCount = prompt("Tickets cost $" + perPerson + " per person. \nHow many people are in your party?"); //This line asks the user how many people are attending the TSO concert and stores the user data in a variable;
 var totalCost = headCount * perPerson; // This line multiplies the cost per person times the amount of people going.
 var displayTotal = "The total cost for " + headCount + " tickets is $" + totalCost + "."; // This creates a string concatenated with the relevant variables
-alert(displayTotal);
+alert(displayTotal); // This displays the information for the user
+console.log (displayTotal); // This displays the information in the console (for assignment purposes only)
