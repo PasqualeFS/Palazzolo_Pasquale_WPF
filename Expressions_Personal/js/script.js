d1c0119fb312b4910e3f63b04615c7089e01380e
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-04 20:21:27 -0500

    Alert, variable, prompt added

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,5 @@
 // This is for my Personal Expressions assignment
+
+alert("Trans-Siberian Orchestra is playing at Amway Arena on 12/14/2014!"); // This line starts an alert to click through.
+var perPerson = 55.00; // This line stores the price per person for a single ticket to TSO
+var headCount = prompt("Tickets cost $" + perPerson + " per person. \nHow many people are in your party?"); //This line asks the user how many people are attending the TSO concert and stores the user data in a variable;
