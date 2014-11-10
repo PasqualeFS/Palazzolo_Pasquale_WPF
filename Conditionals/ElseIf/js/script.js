4ceb2a95faa556ce68414bd496e37ab8a01ba475
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 17:32:45 -0500

    Added prompt
    Just wanted to play using last week’s prompt technique. Now the code
    asks the user to type in their height and then stores it in the
    kidHeight variable.

--- Conditionals/ElseIf/js/script.js
+++ Conditionals/ElseIf/js/script.js
@@ -1,6 +1,6 @@
 // Goes with the Else If Screencast
 
-var kidHeight = 52;
+var kidHeight = prompt("What is your height?");
 var minHeight = 48;
 var wParentHeight = 45; // the height of the kid with the parent
 
