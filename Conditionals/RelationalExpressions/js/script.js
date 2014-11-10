386d53b511cc9e8851be95d4ec64a5b895afbc78
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 16:19:55 -0500

    Created Relational Expressions folder and code
    Folder layout for Relational Expressions screencast and starting JS
    code.

---
+++ Conditionals/RelationalExpressions/js/script.js
@@ -0,0 +1,9 @@
+// JS for Relational Expressions Screencast
+
+var kidHeight = 30;
+
+// if the child is tall enough, print to the console "you can ride!"
+if(){
+    // code performed if child is tall enough
+    console.log("You can ride the coaster!");
+}
