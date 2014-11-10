a6e4d5d8b36a8207a38f6e8e8a8d513ba7eb6e5c
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 16:23:24 -0500

    Tested kidHeight variable and if statement
    Tested kidHeight at 30 to see if it prints to the console.  Since 30 is
    less than 48, it did not print.

--- Conditionals/RelationalExpressions/js/script.js
+++ Conditionals/RelationalExpressions/js/script.js
@@ -2,8 +2,9 @@
 
 var kidHeight = 30;
 
-// if the child is tall enough, print to the console "you can ride!"
-if(){
+// if the child is old enough, print to the console "you can ride!"
+// if the kid is over 48 inches in height
+if(kidHeight > 48){
     // code performed if child is tall enough
     console.log("You can ride the coaster!");
 }
