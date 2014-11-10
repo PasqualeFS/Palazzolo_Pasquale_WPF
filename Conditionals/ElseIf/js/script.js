bea18716039711b378bb7f215b3c089e153fc62f
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 17:30:38 -0500

    Added if else. Changed else
    Made additions and changes to the code to allow for three conditions.

--- Conditionals/ElseIf/js/script.js
+++ Conditionals/ElseIf/js/script.js
@@ -1,15 +1,18 @@
 // Goes with the Else If Screencast
 
-var kidHeight = 48;
+var kidHeight = 52;
 var minHeight = 48;
-
+var wParentHeight = 45; // the height of the kid with the parent
 
 // if the child is old enough, print to the console "you can ride!"
 // if the kid is over 48 inches in height
 if(kidHeight > minHeight){
-    // code performed if condition is true
+    // you can ride
     console.log("You can ride the coaster!");
+} else if(kidHeight > wParentHeight){
+    // you can ride with a parent present
+    console.log("You can ride but only with a parent present.");
 } else {
-    // code performed if condition is false
+    // Sorry. You have growing to do.
     console.log("Sorry, kid! You've got some growing to do first!");
 }
