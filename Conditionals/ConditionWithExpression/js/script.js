3f60a55c8be3f18ddfe65abdae1da6b37a09a680
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 16:37:21 -0500

    Converted value to var
    removed value of +2 from the if statement.  Created sneakerLift
    variable and added it to the if statement. Prints to the console
    properly.

--- Conditionals/ConditionWithExpression/js/script.js
+++ Conditionals/ConditionWithExpression/js/script.js
@@ -2,10 +2,11 @@
 
 var kidHeight = 47;
 var minHeight = 48;
+var sneakerLift = 2;
 
 // if the child is old enough, print to the console "you can ride!"
 // if the kid is over 48 inches in height
-if(kidHeight + 2 > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     // code performed if child is tall enough
     console.log("You can ride the coaster!");
 }
