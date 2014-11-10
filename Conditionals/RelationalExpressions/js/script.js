90a3794155e3f21545a3bde3a0d70f7cb32cf013
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 16:26:29 -0500

    Added variable
    added minHeight variable and had the if statement compare kidHeight to
    minHeight and print to the console.

--- Conditionals/RelationalExpressions/js/script.js
+++ Conditionals/RelationalExpressions/js/script.js
@@ -1,10 +1,11 @@
 // JS for Relational Expressions Screencast
 
 var kidHeight = 50;
+var minHeight = 48;
 
 // if the child is old enough, print to the console "you can ride!"
 // if the kid is over 48 inches in height
-if(kidHeight > 48){
+if(kidHeight > minHeight){
     // code performed if child is tall enough
     console.log("You can ride the coaster!");
 }
