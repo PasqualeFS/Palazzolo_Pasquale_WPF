eed9e69e0245fe29e629c4d58c6d83d469efca97
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 17:10:03 -0500

    Added second if statement
    Additional if statement for kids under the height of 48 inches.

--- Conditionals/IfAndElse/js/script.js
+++ Conditionals/IfAndElse/js/script.js
@@ -1,2 +1,15 @@
 // Goes with the If and Else Screencast
 
+var kidHeight = 50;
+var minHeight = 48;
+
+
+// if the child is old enough, print to the console "you can ride!"
+// if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+    // code performed if condition is true
+    console.log("You can ride the coaster!");
+}
+if (kidHeight < minHeight){
+    console.log("Sorry, kid! You've got some growing to do first!");
+}
