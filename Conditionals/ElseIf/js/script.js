1f7dc78502579d4adbec79daaa9cc8e6511b293b
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 17:17:08 -0500

    Folders for Else If Screencast
    Added folders and files for Else If Screencast

---
+++ Conditionals/ElseIf/js/script.js
@@ -0,0 +1,15 @@
+// Goes with the Else If Screencast
+
+var kidHeight = 48;
+var minHeight = 48;
+
+
+// if the child is old enough, print to the console "you can ride!"
+// if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+    // code performed if condition is true
+    console.log("You can ride the coaster!");
+} else {
+    // code performed if condition is false
+    console.log("Sorry, kid! You've got some growing to do first!");
+}
