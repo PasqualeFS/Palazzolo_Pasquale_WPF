d65a085aa4161acd70108e066a8f5cc28ba35e35
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 16:13:05 -0500

    Added and tested boolean and if statement
    Tested boolean as true.

--- Conditionals/ConditionalsExample/js/script.js
+++ Conditionals/ConditionalsExample/js/script.js
@@ -1,3 +1,10 @@
-/**
- * Created by OldPatman on 11/10/14.
- */
+// This goes with the Conditionals Example Screencast
+// Conditionals Logic
+
+var oldEnough = true;
+
+// if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    // code performed if the condition is true
+    console.log("You can ride the coaster!");
+}
