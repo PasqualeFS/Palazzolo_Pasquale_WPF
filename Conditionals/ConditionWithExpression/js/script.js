652305e99fe5d8fe804744226b827f981254b441
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 16:34:32 -0500

    Updated and added
    Updated files.  Added +2 to kidHeight variable within the if statement.
     Still doesn’t exceed 48 so it did not print to the console.

--- Conditionals/ConditionWithExpression/js/script.js
+++ Conditionals/ConditionWithExpression/js/script.js
@@ -1,11 +1,11 @@
-// JS for Relational Expressions Screencast
+// JS for Condition with Expressions Screencast
 
 var kidHeight = 30;
 var minHeight = 48;
 
 // if the child is old enough, print to the console "you can ride!"
 // if the kid is over 48 inches in height
-if(kidHeight > minHeight){
+if(kidHeight + 2 > minHeight){
     // code performed if child is tall enough
     console.log("You can ride the coaster!");
 }
