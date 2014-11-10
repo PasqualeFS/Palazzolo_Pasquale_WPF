7e2d3ee12dcf1c1ef1769c528f8ba1b01ac4c811
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 16:30:26 -0500

    Folders for Condition With Expressions Screencast
    Added folder layout to follow this screencast.

---
+++ Conditionals/ConditionWithExpression/js/script.js
@@ -0,0 +1,11 @@
+// JS for Relational Expressions Screencast
+
+var kidHeight = 30;
+var minHeight = 48;
+
+// if the child is old enough, print to the console "you can ride!"
+// if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+    // code performed if child is tall enough
+    console.log("You can ride the coaster!");
+}
