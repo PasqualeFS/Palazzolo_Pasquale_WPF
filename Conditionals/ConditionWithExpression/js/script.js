abed772899d313de0dadf5ea532a3696c3f19bd9
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 16:35:28 -0500

    Changed kidHeight
    Increased kidHeight to 47.  Even though it’s less than 48, the +2 in
    the if statement totals 49 and it exceeds the minHeight. Printed
    successfully to the console.

--- Conditionals/ConditionWithExpression/js/script.js
+++ Conditionals/ConditionWithExpression/js/script.js
@@ -1,6 +1,6 @@
 // JS for Condition with Expressions Screencast
 
-var kidHeight = 30;
+var kidHeight = 47;
 var minHeight = 48;
 
 // if the child is old enough, print to the console "you can ride!"
