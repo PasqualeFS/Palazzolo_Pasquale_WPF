397bef079a9f1a08b44aa6ede45f9cfad911edbf
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-11 07:01:04 -0500

    Added vars and conditional
    added two variables and standard conditional. Printed to console to
    test.

--- Conditionals/Ternaries/js/script.js
+++ Conditionals/Ternaries/js/script.js
@@ -19,20 +19,35 @@ if(condition){
 
  */
 
+
+/*
 var gpa = 48;
 
 // if the gpa is over the min 2.0 score, this student can graduate
 // this conditional statement is written in standard format
 
-/*
 if(gpa > 2.0){
     console.log("You can graduate!");
 } else{
     console.log("GPA is too low!");
 }
-*/
+
 
 // This is the same conditional with a ternary operator
 
 (gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
+*/
+
+var age = 11;
+var book;
+
+// if child is under 10, they get green eggs and ham, otherwise they get The Time Machine
+
+if(age < 10){
+     book = "Green Eggs and Ham";
+} else {
+    book = "The Time Machine";
+}
+
+console.log(book);
 
