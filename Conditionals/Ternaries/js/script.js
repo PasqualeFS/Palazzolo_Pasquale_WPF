4a4db72f479370440f8b68f6888aaa1a3bf4bacf
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-11 06:51:49 -0500

    Updated comments
    Added more notes about the comparison between standard if statement and
    difference when using ternary operator

--- Conditionals/Ternaries/js/script.js
+++ Conditionals/Ternaries/js/script.js
@@ -2,3 +2,22 @@
 
 /* It's called "Ternary" because this operator requires three components.  It uses a ? */
 
+/* A standard conditional/if statement looks like this:
+
+
+if(condition){
+    do if true;
+} else {
+    do if false;
+}
+
+ */
+
+/* A Ternary operator would look like this:
+
+(condition) ? do if true : do if false;
+
+ */
+
+
+
