878cc60379daed2f045550e2945e6e64bc71899e
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 20:59:15 -0500

    Changed boolean
    Changed wonLottery to false. Since iPhone price is higher than budget
    and wonLotter var is false, we can’t buy the phone.

--- Conditionals/LogicalOperators/js/script.js
+++ Conditionals/LogicalOperators/js/script.js
@@ -2,7 +2,7 @@
 
 var budget = 300;
 var iPhonePrice = 499.99;
-var wonLottery = true;
+var wonLottery = false;
 
 // if the price of the phone is less than our budget AND if the paycheck is over 300
 // && operator must always go between relational expressions or booleans
