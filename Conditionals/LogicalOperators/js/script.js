cfed6b97139a051d6128a756f69e95657df8f33a
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 20:51:31 -0500

    Added AND operator and moved paycheck conditional
    removed second if statement and combined the conditional into the first
    if statement using the && operator.  Tested in browser.

--- Conditionals/LogicalOperators/js/script.js
+++ Conditionals/LogicalOperators/js/script.js
@@ -5,12 +5,8 @@ var iPhonePrice = 199.99;
 var paycheck = 200;
 
 // if the price of the phone is less than our budget AND if the paycheck is over 300
-if(iPhonePrice < budget){
+if(iPhonePrice < budget && paycheck > 300){
     console.log("We can buy the phone!");
 } else{
     console.log("No phone for you!!");
 }
-if (paycheck > 300){
-    console.log("We can buy the phone!");
-} else{
-    console.log("No phone for you!!");
