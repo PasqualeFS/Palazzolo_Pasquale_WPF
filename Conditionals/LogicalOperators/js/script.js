3a6be484afad1a0a6160df024f0fbd7910431bc8
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 20:45:46 -0500

    Completed conditional and tested
    Connected vars to if statement and tested in the browser.

--- Conditionals/LogicalOperators/js/script.js
+++ Conditionals/LogicalOperators/js/script.js
@@ -4,7 +4,7 @@ var budget = 300;
 var iPhonePrice = 199.99;
 var paycheck = 200;
 
-if(){
+if(iPhonePrice < budget){
     console.log("We can buy the phone!");
 } else{
     console.log("No phone for you!!");
