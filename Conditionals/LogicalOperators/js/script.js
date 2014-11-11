34482f2176de25e1bddfc65fe34498ca22d8e3a3
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-10 20:44:13 -0500

    Added vars and empty conditionals

--- Conditionals/LogicalOperators/js/script.js
+++ Conditionals/LogicalOperators/js/script.js
@@ -1,1 +1,11 @@
 // For Logical Operators Screencast
+
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
+
+if(){
+    console.log("We can buy the phone!");
+} else{
+    console.log("No phone for you!!");
+}
