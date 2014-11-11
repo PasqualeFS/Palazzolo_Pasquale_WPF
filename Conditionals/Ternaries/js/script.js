8f4e38f27cc865ec143106a4ea5c312f927f826e
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-11 06:58:02 -0500

    Added ternary operator
    commented out standard conditional statement and added conditional with
    ternary operator. Tested in browser.

--- Conditionals/Ternaries/js/script.js
+++ Conditionals/Ternaries/js/script.js
@@ -19,5 +19,20 @@ if(condition){
 
  */
 
+var gpa = 48;
 
+// if the gpa is over the min 2.0 score, this student can graduate
+// this conditional statement is written in standard format
+
+/*
+if(gpa > 2.0){
+    console.log("You can graduate!");
+} else{
+    console.log("GPA is too low!");
+}
+*/
+
+// This is the same conditional with a ternary operator
+
+(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
 
