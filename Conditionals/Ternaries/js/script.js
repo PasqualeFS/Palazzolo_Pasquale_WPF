b38b68bcdf5d9adbc4d36ae2464f12d2846de498
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-11 06:49:25 -0500

    Updated HTML, CSS and JS files
    linked files, added titles and comments

--- Conditionals/Ternaries/js/script.js
+++ Conditionals/Ternaries/js/script.js
@@ -1,3 +1,4 @@
-/**
- * Created by OldPatman on 11/10/14.
- */
+// This goes with the Ternary Operators Screencast
+
+/* It's called "Ternary" because this operator requires three components.  It uses a ? */
+
