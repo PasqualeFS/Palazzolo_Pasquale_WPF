15c35899dfcaf71cac282713a88989c469b28f97
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-12 18:22:21 -0500

    Printed recipe to console
    Created console.log with recipe and tested it in the console

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,2 +1,3 @@
 // This file goes with my Personal Conditional Calculator
 
+console.log ("Mama's Italian Chicken Soup\n\nThis is a standard recipe for my mother's chicken soup. It serves 8 people. In order to make this recipe, you will need the following ingredients:\n\n3 Boneless Skinless Chicken Breasts\n4 Celery Stalks, sliced\n1 lb Baby Carrots\n1 Sweet Onion\n16 oz. Chicken Stock\n1 Small Can of Tomato Sauce\n1 Tbsp Dried Parsley\n1 Tbsp Pureed Basil\nSalt and Pepper, to taste");
