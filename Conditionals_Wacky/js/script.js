e3fdfde766bc56cf047aaa2b6a1a29d73288913c
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-13 08:23:25 -0500

    Added variables and ternary operator
    Added three vars and one ternary operator. Printed to console to test.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,2 +1,8 @@
 // This script file is part of the Conditionals: Wacky assignment
 
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var yesNo = prompt("Greetings, Professor " + lastName + ". May I call you " + firstName + "?\n\nPlease respond with \"yes\" or \"no\"\nYour response is case sensitive.");
+mayI = (yesNo === "yes") ? "Great! I prefer to be socially casual." : "My apologies, Professor. I'll keep it professional.";
+
+console.log(mayI);
