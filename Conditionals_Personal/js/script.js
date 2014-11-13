9b6648a0208cb4d45ec18ee552122f3564775cab
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-12 21:28:33 -0500

    Simplified
    I simplified this to use a smaller recipe.  Added four variables with
    basic recipe details.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,2 +1,8 @@
 // This file goes with my Personal Conditional Calculator
 
+var oneSvPrunes = 3; // This var contains a single serving of prunes
+var oneSvChocolate = 31.25; // This var contains a single serving of chocolate
+var StandardYield = 8; // This var is the standard yield of one batch
+var DoubleYield = 16; // This var is the yield of two batches
+var TripleYield = 24; // This var is the yield of three batches
+
