be7d608dd6cccc74044cb1670434f01371792f7a
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-13 08:55:56 -0500

    Variables, concatenation and testing
    Added several more variables that will store information to help
    complete the song in the final concatenated string. Prints result to
    console and alerts user.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -12,5 +12,29 @@ var yesNo = prompt("Greetings, Professor " + lastName + ". May I call you " + fi
 // This ternary operator creates a result based on the user data stored in the yesNo variable
 mayI = (yesNo === "yes") ? "Great! I prefer to be socially casual." : "My apologies, Professor. I'll keep it professional.";
 
-// This prints the result of the mayI variable to the console.
-console.log(mayI);
+// This continues the conversation and then asks how many people will be drinking
+var drinkerCount = prompt(mayI + "\n\nYou may call me Joshua.\n\nShall we sing a drinking song?\n\nHow many people will be drinking?");
+
+// This continues conversation asking user how many drinks per person
+var drinksCount = prompt("How many drinks per person?");
+
+// This continues conversation asking user what their favorite drink is
+var favoriteDrink = prompt("What is your favorite drink?");
+
+// This variable multiplies the amount of drinkers times the amount of drinks and stores the total
+var totalDrinks = drinkerCount * drinksCount;
+
+// This variable just stores a single drink
+var oneDown = 1;
+
+// This variable reduces the totalDrinks variable by the amount stored in the oneDown variable
+var takeOne = totalDrinks - oneDown;
+
+// This concatenates the following strings with several of the above stored variables to make a song
+var ninetyNineBottles = totalDrinks + " bottles of " + favoriteDrink + " on the wall\n" + totalDrinks + " bottles of " + favoriteDrink + "!\nTake " + oneDown + " down. Pass it around.\n" + takeOne + " bottles of " + favoriteDrink + " on the wall!";
+
+// This displays the ninetyNineBottle variable for the user
+alert(ninetyNineBottles);
+
+// This prints the result of the ninetyNineBottles variable to the console.
+console.log(ninetyNineBottles);
