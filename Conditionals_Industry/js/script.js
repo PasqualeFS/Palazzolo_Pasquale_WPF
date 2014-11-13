07ee82baafd66735c30338d53a7ac78bc534dbfc
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-12 22:42:27 -0500

    Added ternary operator
    Used a ternary operator to output a result depending on the amount
    entered by the user.  This is ready for instructor review.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -9,6 +9,17 @@ alert("Good morning, " + yourName + "! Daryl had to take the day off.\n\nHis dog
 // This variable stores the amount of email jobs in the work queue
 var emailJobs = 32;
 
-// This variable states the amount of jobs and prompts the user asking what amount they can work on.
-var yourJobs = prompt("There are currently " + emailJobs + " jobs in the queue. With your current work load, how many jobs can you take?");
+// This variable will hold the result of the ternary operator below.
+var work;
 
+//This shows how much I'm working on
+var myJobs = 8;
+
+// This variable states the amount of jobs and prompts the user asking what amount of work they currently have.
+var yourJobs = prompt("There are currently " + emailJobs + " jobs in the queue. How many jobs are you currently working on?");
+
+// This ternary operator checks to see if the user has less tasks than I do. If they have more, I'll take on the bulk.  If they have less, they can work on a few more.
+work = (yourJobs < myJobs) ? "You have less tasks than me. Take a few more from the queue please." : "That\'s a lot. You have more tasks than me. Don\'t stress! I\'ll take the bulk of the work today.";
+
+alert(work); // This alerts the user with one of the ternary operator results.
+console.log(work); // This prints the ternary operator result to the console.
