5ccf1838ef7c920c9e14e36e8374ea2d1dbbf890
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-18 18:45:35 -0500

    Added notes
    Commented notes go along to explain the difference between a function
    and a procedure

--- Functions/Returning_Values/js/script.js
+++ Functions/Returning_Values/js/script.js
@@ -1,3 +1,22 @@
-/**
- * Created by OldPatman on 11/18/14.
- */
+/* This JS goes along with the Returning Values Screencast */
+
+var total = calcArea(30, 20);
+
+function calcArea(w, h){
+    var area = w * h;
+    return area; // function returns area back to calcArea
+}
+
+console.log(total);
+
+// Functions return values
+function calcAreaF(width, height){
+    var area = width * height;
+    return area;
+}
+
+//Procedure does not return anything
+function calcAreaP(width, height){
+    var area = width * height;
+    console.log(area); //no return
+}
