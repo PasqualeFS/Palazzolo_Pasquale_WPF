596040ba218b42794267129b5692cc9ed1ce2409
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-18 18:48:43 -0500

    Created folders and files
    These go along with the Anonymous functions screencast

---
+++ Functions/Anonymous_Functions/js/script.js
@@ -0,0 +1,11 @@
+/* This JS goes with the Anonymous Functions Screencast
+ var functionName = function(){
+    //code the funtion runs
+ }
+
+ functionName();
+*/
+
+function(){
+    //code to run
+}
