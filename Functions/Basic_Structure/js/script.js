c3d64c0c5ec730d77d19f63f446aad47c9e48b03
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-18 08:43:55 -0500

    Added notes and code
    Added more notes to go along with the Variable Scope screencast. Tested
    a console.log outside on a variable outside of the function.

--- Functions/Basic_Structure/js/script.js
+++ Functions/Basic_Structure/js/script.js
@@ -7,11 +7,15 @@ function functionName() {
     // code that the function runs
 }
 
- */
+
 
 function outptMsg(){
     console.log("Hello World");
 }
+
+ */
+
+var width = 5; // Even though there are two widths now, the function is going to prefer to run the code within it.
 
 function calcArea(){
     var width = 20;
@@ -22,4 +26,6 @@ function calcArea(){
 
 // The above functions won't run unless they are invoked or called.  Just type the function with parentheses to run it.
 
-calcArea();
+calcArea(); // This calls the calcArea function and runs it in the console.
+console.log(width); // This will call any "width" variables OUTSIDE of the written functions.  Testing in the console will result in seeing "5" instead of the "600 output from the function
+
