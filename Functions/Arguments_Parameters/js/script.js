84d15c016e9422bd94a2855108a8f208e3c3a0dd
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-18 12:35:16 -0500

    Added notes
    Put in a comment containing notes on the structure used for arguments
    and parameters

--- Functions/Arguments_Parameters/js/script.js
+++ Functions/Arguments_Parameters/js/script.js
@@ -1,1 +1,13 @@
 /* This goes along with the Arguments and Parameters Screencast */
+
+/*
+Structure:
+
+// This is when calling a function
+funcName (argument1, argument2);
+
+// This is a defined function with parameters
+function funcName (parameter1, parameter2){
+    //Code the function runs
+}
+ */
