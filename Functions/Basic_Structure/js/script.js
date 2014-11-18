9db958c10c94e96726de7b85919f59cf43f3ef7f
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-18 08:34:30 -0500

    Added calcArea function

--- Functions/Basic_Structure/js/script.js
+++ Functions/Basic_Structure/js/script.js
@@ -12,3 +12,11 @@ function functionName() {
 function outptMsg(){
     console.log("Hello World");
 }
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
