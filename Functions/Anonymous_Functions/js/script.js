5885679ab9c56746dea0a89c7cc6a749a1532fa2
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-18 18:52:41 -0500

    Added anonymous function

--- Functions/Anonymous_Functions/js/script.js
+++ Functions/Anonymous_Functions/js/script.js
@@ -1,11 +1,18 @@
 /* This JS goes with the Anonymous Functions Screencast
  var functionName = function(){
-    //code the funtion runs
+    //code the function runs
  }
 
  functionName();
 */
 
-function(){
-    //code to run
+
+
+var calcArea = function(width, height){
+    var area = width * height;
+    return area;
 }
+
+var a = calcArea(20, 30);
+
+console.log(a);
