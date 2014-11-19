0530007a5426b9ac931198cd7eafba2134b55c57
PasqualeFS <pvpalazzolo@fullsail.edu>
2014-11-19 18:56:18 -0500

    Added two variables and finalized first If statement
    Set up two variables to store information on whether box is bigger or smaller and concatenated the bigger box variable with a string in an if statement.

--- 4_WeekFour/Assignments/Functions_Personal/js/script.js
+++ 4_WeekFour/Assignments/Functions_Personal/js/script.js
@@ -2,6 +2,18 @@
 /* This JS is for Pasquale Palazzolo's Personal Function Calculator */
 
 //This alert tells the user to open the console for their browser
+<<<<<<< HEAD
+alert("If the console is not already open, please open\nit and refresh before we begin");
+
+// This alert sets up the personal story for the calculator
+alert("With all the farm to table restaurants popping up, it seems like a good choice\nto grow your own herbs instead of buying them from a purveyor.\n\nWe've purchased a box for planting an herb garden but need to figure out if\nyou have enough counter space to hold it.\n\n");
+
+// This asks the user for a width
+var counterWidth = prompt("What is the width of your counter space in inches?");
+
+// This asks the user for a length
+var counterLength = prompt("What is the length of your counter space in inches?");
+=======
 alert("If the console is not already open, please open\nit and refresh before we begin"); 
 
 // This alert sets up the personal story for the calculator
@@ -12,6 +24,7 @@ var counterWidth = prompt("What is the width of your counter space in inches?");
 
 // This asks the user for a length
 var counterLength = prompt("What is the length of your counter space in inches?"); 
+>>>>>>> FETCH_HEAD
 
 // This calculates the perimeter based on user entries.
 var counterPerimeter = Number(counterWidth) * 2 + Number(counterLength) * 2;
@@ -40,14 +53,25 @@ var herbBoxPerimeter = function(width, length){
 //The next two lines invoke both functions
 var storeBoxArea = herbBoxArea(4, 8);
 var storeBoxPerimeter = herbBoxPerimeter(4,8);
+var boxBigger = storeBoxPerimeter - counterPerimeter;
+var boxSmaller = counterPerimeter - storeBoxPerimeter;
 
 //console.log(storeBoxPerimeter);
 //console.log(storeBoxArea);
 
 if (storeBoxPerimeter > counterPerimeter) {
+<<<<<<< HEAD
+    console.log("Since the box we bought has a perimeter of " + storeBoxPerimeter + " it exceeds the counterspace by " + boxBigger + " inches and won't fit well on the counter.");
+} else if (storeBoxPerimeter === counterPerimeter) {
+    console.log("The box fits perfectly on the counter");
+} else {
+    console.log("The box fits with room to spare");
+}
+=======
 	console.log("The box is too big for the counter");
 } else if (storeBoxPerimeter === counterPerimeter) {
 	console.log("The box fits perfectly on the counter");
 } else {
 	console.log("The box fits with room to spare");
 }
+>>>>>>> FETCH_HEAD
