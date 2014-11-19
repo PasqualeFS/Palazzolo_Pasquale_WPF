45d1195ab6280e9b3ceca1b789dcbb45d65227a5
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-19 12:41:23 -0500

    Changed text in alerts
    Revised some of the alert text within my if else statement.

--- 4_WeekFour/Assignments/Functions_Personal/js/script.js
+++ 4_WeekFour/Assignments/Functions_Personal/js/script.js
@@ -3,14 +3,16 @@
 alert("If the console is not already open, please open\nit and refresh before we begin");
 
 var counterWidth = prompt("What is the width of your counter space in inches?"); // This asks the user for a width
+
 var counterLength = prompt("What is the length of your counter space in inches?"); // This asks the user for a length
 
 var counterPerimeter = Number(counterWidth) + Number(counterWidth) + Number(counterLength) + Number(counterLength); // This calculates the perimeter based on user entries.
 
 if (counterLength < counterWidth){ //This checks to see if the length is shorter than the width and alerts the user
-    alert("Sorry.  Your length needs to equal or exceed your width. Please refresh your browser and start again")
+    alert("A width that exceeds your length? That's a bit odd but the perimeter is " + counterPerimeter + " inches.")
 } else if (counterLength === counterWidth) { //This checks to see if the length and width are equal and alerts the user.
-    alert("Ahh, a square shaped counter space.\n\nThe perimeter of your square counter is " + counterPerimeter + " inches.\n\nLet's proceed.")
-} else { //This alerts the user and continues on if all the length is greater than the width and they are not equal.
-    alert("Great!  The perimeter of your counter space is " + counterPerimeter + " inches.");
+    alert("Ahh, a square shaped counter space.\n\nThe perimeter of your square counter is " + counterPerimeter + " inches.")
+} else { //This alerts the user if the length is greater than the width and they are not equal.
+    alert("Great! The perimeter of your counter space is " + counterPerimeter + " inches.")
 }
+
