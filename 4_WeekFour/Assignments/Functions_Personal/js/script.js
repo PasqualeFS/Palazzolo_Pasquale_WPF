acdfda02d403a34941a106082da816e48546fe42
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-19 08:43:49 -0500

    Created basic equation
    used vars and user data to create the first part of this calculator.

--- 4_WeekFour/Assignments/Functions_Personal/js/script.js
+++ 4_WeekFour/Assignments/Functions_Personal/js/script.js
@@ -1,3 +1,9 @@
-/**
- * Created by OldPatman on 11/18/14.
- */
+/* This JS is for Pasquale Palazzolo's Personal Function Calculator */
+
+alert("If the console is not already open, please open\nit and refresh before we begin");
+
+var counterWidth = prompt("What is the width of your counter space in inches?");
+var counterLength = prompt("What is the length of your counter space in inches?");
+
+var counterPerimeter = Number(counterWidth) + Number(counterWidth) + Number(counterLength) + Number(counterLength);
+
