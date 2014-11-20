7ec6dd4be88cf57b32c8ba1d9dad71d66d062cf3
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-19 20:46:37 -0500

    Added variables
    three new variables prompting the user for numbers

--- 4_WeekFour/Assignments/Functions_Industry/js/script.js
+++ 4_WeekFour/Assignments/Functions_Industry/js/script.js
@@ -1,7 +1,21 @@
 /*  This is the JS file for Pasquale Palazzolo's Industry Expressions Calculator */
 
 alert("Once upon a time there live a small email marketing department");
-alert("Daryl the brave led the troops.\nPasquale utilized inbox sorcery.\n\'Twas the fair maiden, Kadri, who was busiest of them all.");
+alert("Daryl the brave led the troops. Pasquale utilized inbox sorcery.\n\'Twas the fair maiden, Kadri, who was busiest of them all.");
 alert("Each day, the three would gaze into Ye Olde Book of Tasks and see what the future held for them.");
-alert("On this particular day, there was an excessive about of Tasks in the book. They would all be very busy.")
+alert("On this particular day, there was an excessive amount of Tasks in the book. They would all be very busy.")
 
+
+var PasqualeTasks = prompt("Gaze in to Ye Olde Book of Tasks. How many tasks doth Pasquale have?\n\nPlease enter a large number.");
+var DarylTasks = prompt("and what does thou see in store for Sir Daryl?\n\nPlease enter a large number");
+var KadriTasks = prompt("Surely they will fail in battle against this foe.  How grim are things for the maiden, Kadri?\n\nPlease enter the largest number")
+/*
+calcArea(30, 20);
+
+// Since 30 is the first argument, it's going to be stored in the "w" parameter. 20 will be stored in the "h" parameter
+function calcArea(pas, h){
+    var totalTasks = ;
+    console.log(area);
+}
+
+    */
