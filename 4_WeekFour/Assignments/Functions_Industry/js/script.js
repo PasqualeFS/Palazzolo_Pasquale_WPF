b749fbb362c9e474e2e8362c7e117b58f9fe69a3
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-19 20:37:03 -0500

    Created alerts
    These alerts set up the story for this calculator.

--- 4_WeekFour/Assignments/Functions_Industry/js/script.js
+++ 4_WeekFour/Assignments/Functions_Industry/js/script.js
@@ -1,2 +1,7 @@
 /*  This is the JS file for Pasquale Palazzolo's Industry Expressions Calculator */
 
+alert("Once upon a time there live a small email marketing department");
+alert("Daryl the brave led the troops.\nPasquale utilized inbox sorcery.\n\'Twas the fair maiden, Kadri, who was busiest of them all.");
+alert("Each day, the three would gaze into Ye Olde Book of Tasks and see what the future held for them.");
+alert("On this particular day, there was an excessive about of Tasks in the book. They would all be very busy.")
+
