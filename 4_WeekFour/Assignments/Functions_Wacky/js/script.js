75576b0b95f2adc446cfacc3f7b6c32bf01e0f4b
Pasquale Palazzolo <pvpalazzolo@fullsail.edu>
2014-11-19 21:53:29 -0500

    Added alerts, variables, function and if elses

--- 4_WeekFour/Assignments/Functions_Wacky/js/script.js
+++ 4_WeekFour/Assignments/Functions_Wacky/js/script.js
@@ -1,3 +1,28 @@
-/**
- * Created by OldPatman on 11/18/14.
- */
+/* This JS file is for Pasquale Palazzolo's Wacky Functions calculator */
+
+alert("Good morning, Major Tom.  I notice you're standing there alone.");
+var comeHome = prompt("Would you like to come home? Please enter \'y\' or \'n\'.");
+
+function move() {
+    window.location = 'http://youtu.be/0_O3wgiymXg';
+}
+
+if (comeHome === "y"){
+    alert("The ship is waiting.");
+} else if (comeHome === "n") {
+    alert("...the ship is waiting. All systems are go, are you sure?!");
+} else {
+    alert("Does not compute.  Please refresh the program and choose \'y\' or \'n\'.")
+    alert("Malfunction");
+    alert("System Override");
+    alert("The rowers keep on rowing");
+    alert("but there's no way of knowing");
+    alert("Does not compute");
+    alert("error! error!");
+    alert("ever seen a green smurf?");
+    alert("Pork chop sandwiches");
+    alert("My god...it's full of stars");
+    alert("ever eaten a $48 steak in a haunted castle in tampa? I have");
+    move();
+}
+
